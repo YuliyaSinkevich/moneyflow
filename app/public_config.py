3697b3bbe14f17bf83c211fa6c316b02d3797d1a
Alexandr Topilski <atopilski@gmail.com>
2018-06-12 13:13:53 +0300

    Create public_config.py

---
+++ app/public_config.py
@@ -0,0 +1,3 @@
+PUBLIC_CONFIG = {'site': {'title': 'MoneyFlow', 'keywords': 'money,flow,save'},
+                 'company': {'title': 'FastoGT', 'domain': 'http://fastogt.com'},
+                 'support': {'contact_email': 'support@fastogt.com'}}
