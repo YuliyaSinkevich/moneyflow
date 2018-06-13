aa0c87946555edb3c249c3ab4cdbaf0eb2b5ed11
topilski <atopilski@gmail.com>
2018-06-13 12:13:53 -0400

    Without index

--- app/home/user_loging_manager.py
+++ app/home/user_loging_manager.py
@@ -52,8 +52,8 @@ class MoneyEntry(db.EmbeddedDocument):
 class Settings(db.EmbeddedDocument):
     currency = db.StringField(default=constants.DEFAULT_CURRENCY)
     locale = db.StringField(default=constants.DEFAULT_LOCALE)
-    start_date = db.DateTimeField(required=True, default = utils.year_month_min_date(datetime.today()))
-    end_date = db.DateTimeField(required=True, default = utils.year_month_max_date(datetime.today()))
+    start_date = db.DateTimeField(required=True, default=utils.year_month_min_date(datetime.today()))
+    end_date = db.DateTimeField(required=True, default=utils.year_month_max_date(datetime.today()))
 
 
 class User(UserMixin, db.Document):
@@ -62,7 +62,7 @@ class User(UserMixin, db.Document):
         ACTIVE = 1
         BANNED = 2
 
-    meta = {'collection': 'users'}
+    meta = {'collection': 'users', 'auto_create_index': False}
     email = db.StringField(max_length=30, required=True)
     password = db.StringField(required=True)
     created_date = db.DateTimeField(default=datetime.now)
