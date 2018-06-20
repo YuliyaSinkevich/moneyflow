abefb99adc2bddeca438fc42b1419ebd491d9836
YuliyaSinkevich <36645108+YuliyaSinkevich@users.noreply.github.com>
2018-06-20 18:27:08 +0300

    Advantages (#7)

--- app/static/assets/js/script.js
+++ app/static/assets/js/script.js
@@ -1,9 +1,2 @@
-﻿$(document).ready(function () {
-    $("#main-top-navbar").on("click", "a", function (event) {
-        event.preventDefault();
-        var id = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 500);
-    });
-});
+﻿
 
