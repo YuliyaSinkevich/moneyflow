050390ffb896fee3ff94151151e2027fe0e92c19
topilski <atopilski@gmail.com>
2018-06-25 09:55:44 -0400

    Fix cert

--- app/utils.py
+++ app/utils.py
@@ -2,6 +2,7 @@ import calendar
 from datetime import datetime
 
 import json
+import ssl
 
 from validate_email import validate_email
 from urllib.request import urlopen
@@ -30,7 +31,8 @@ def is_valid_email(email: str, check_mx: bool) -> bool:
         return False
 
     validate_url = 'https://open.kickbox.com/v1/disposable/' + email
-    response = urlopen(validate_url)
+    context = ssl._create_unverified_context()
+    response = urlopen(validate_url, context=context)
     if response.status != 200:
         return False
 
